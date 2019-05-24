ae306d2d0958681408873e6b8f30dde7d9508794
jerry <jerryhanjj@126.com>
2019-05-24 17:51:43 +0800

    chapter 2, question 1

---
+++ ch02/ex01.cpp
@@ -0,0 +1,11 @@
+// Chapter 1, question 1
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "My first name is Jerry, ";
+	cout << "and my address is HERE !" << endl;
+	return 0;
+}
