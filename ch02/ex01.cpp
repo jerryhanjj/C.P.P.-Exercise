9166a251626b83fc637f3ea0c71f1d9faca1a204
jerry <jerryhanjj@126.com>
2019-05-24 17:50:13 +0800

    Chapter 1, question 2

--- ch02/ex01.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(int argc, char const *argv[])
-{
-	cout << "My first name is Jerry, ";
-	cout << "and my address is HERE !" << endl;
-	return 0;
-}
---
+++ ch02/ex02.cpp
@@ -0,0 +1,17 @@
+// Chapter 1, question 2
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int ilong = 0;
+
+	cout << "please input distance(long):";
+	cin >> ilong;
+
+	cout << "The distance is " << ilong*220 << " yards.";
+	cout << endl;
+
+	return 0;
+}
